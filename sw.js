1207f452a4e03ce0bd9117410ed377b95290cad0
gokhantaylann-TR <gokhantaylann@gmail.com>
2026-01-29 11:05:57 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,10 @@
-self.addEventListener("install",e=>{
- self.skipWaiting();
-});
-
+self.addEventListener("install",()=>self.skipWaiting());
 self.addEventListener("fetch",e=>{
  e.respondWith(
-  caches.open("altay-cache").then(cache=>{
-   return cache.match(e.request).then(resp=>{
-    return resp||fetch(e.request).then(net=>{
-     cache.put(e.request,net.clone());
-     return net;
-    });
-   });
-  })
+  caches.open("altay-cache").then(cache=>
+   cache.match(e.request).then(r=>r||fetch(e.request).then(n=>{
+    cache.put(e.request,n.clone()); return n;
+   }))
+  )
  );
 });
