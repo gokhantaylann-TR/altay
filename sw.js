455d107e265a1a07084e0b9e538f74e78970fc84
gokhantaylann-TR <gokhantaylann@gmail.com>
2026-01-29 18:07:23 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,18 @@
-const CACHE = "altay-v2";
+const CACHE_NAME = "altay-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
 
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ])
-    )
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", e => {
-  e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
-
-
- const d = this.diamonds.create(0,0,"diamond")
-   .setScale(0.25)
-   .setDepth(5);
-
- d.brick = b;
-}
-
-// Elmasları tuğlanın üstünde sabitle
-this.diamonds.children.iterate(d=>{
- if(d && d.brick && d.brick.active){
-   d.x = d.brick.x;
-   d.y = d.brick.y - d.brick.height/2 - 8;
- }
-});
-
-
-
