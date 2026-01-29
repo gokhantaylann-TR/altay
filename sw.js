b200a5d7b7c130c74b03126e64b52738962e6a66
gokhantaylann-TR <gokhantaylann@gmail.com>
2026-01-29 18:22:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,16 @@
-const CACHE_NAME = "altay-v1";
-const ASSETS = [
+const CACHE = "altay-v1";
+const FILES = [
   "./",
   "./index.html",
   "./manifest.json"
 ];
 
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
