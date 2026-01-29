65099d1ab58f6f1d8f8aa7d708be6993f544d714
gokhantaylann-TR <gokhantaylann@gmail.com>
2026-01-29 12:12:17 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,8 @@ self.addEventListener("fetch",e=>{
  e.respondWith(
   caches.open("altay-cache").then(cache=>
    cache.match(e.request).then(r=>r||fetch(e.request).then(n=>{
-    cache.put(e.request,n.clone()); return n;
+    cache.put(e.request,n.clone());
+    return n;
    }))
   )
  );
